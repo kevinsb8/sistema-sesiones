1e204c865dd6586c8327f0c3a96a2b619b3751f9
kevinsb8 <kl792027@gmail.com>
2026-05-22 09:21:21 -0500

    Add basic login form in index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head><title>Login</title></head>
+<body>
+    <form action="login.php" method="POST">
+        <h2>Ingresar al sistema</h2>
+        Usuario: <input type="text" name="user" required>
+        <button type="submit">Entrar</button>
+    </form>
+</body>
+</html>
